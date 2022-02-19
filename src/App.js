e802a5e80a2f0ae1fab81ccb3ef0fc83271b90e7
Aleksandr Romanov <garlife@gmail.com>
2022-02-19 16:08:42 +0300

    added roadmap

--- src/App.js
+++ src/App.js
@@ -3,58 +3,67 @@ import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 import "./App.css";
 import View from "./View";
 import MyHWs from "./MyHWs";
+import GitHub from "./GitHub";
+import roadmap from "./roadmap.png"
 // import { Menu } from 'antd';
 
-
 const Img = () => {
-  return(
-    <div>
+  return (
+    <div style={{ height: "1000 px" }}>
       <h1>Роадмап курса</h1>
-      <img src="https://trello.com/1/cards/61c4c7977522155990570844/attachments/61c4c7977522155990570870/download/image.png" alt="course roadmap image"></img>
+      <img
+        src={roadmap}
+        alt="course roadmap image"
+        style={{ width: "1000 px", height: "1000 px" }}
+      ></img>
     </div>
-  )
-}
+  );
+};
 
 const MenuReact = (props) => {
-  return(
+  return (
     <menu>
-        <BrowserRouter>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Материалы на курсе</Link>
-                </li>
-                <li>
-                  <Link to="/roadmap">Роадмап курса</Link>
-                </li>
-                <li>
-                  <Link to="/works">Мои работы</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </BrowserRouter>
-      </menu>
-  )
-}
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Материалы на курсе</Link>
+              </li>
+              <li>
+                <Link to="/roadmap">Роадмап курса</Link>
+              </li>
+              <li>
+                <Link to="/works">Мои работы</Link>
+              </li>
+              <li>
+                <Link to="/gitIssues">Github Issues</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </BrowserRouter>
+    </menu>
+  );
+};
 
 function App() {
   return (
-    <>
-    <MenuReact />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<View />} />
-        <Route path="/roadmap" element={<Img />} />
-        <Route path="/works" element={<MyHWs />} />
-        {/* <Route path="users" element={<Users />}>
+    <div>
+      <MenuReact />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<View />} />
+          <Route path="/roadmap" element={<Img />} />
+          <Route path="/works" element={<MyHWs />} />
+          <Route path="/gitIssues" element={<GitHub />} />
+          {/* <Route path="users" element={<Users />}>
           <Route path="me" element={<OwnUserProfile />} />
           <Route path=":id" element={<UserProfile />} />
         </Route> */}
-      </Routes>
-    </BrowserRouter>
-    </>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/GitHub.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function GitHub()
+{
+    return(
+        <>
+            <h1>git issues</h1>
+        </>
+    )
+}
