38b431718f33abcb54f288d9278a4ab81bcbe0b7
Aleksandr Romanov <garlife@gmail.com>
2022-02-21 13:13:06 +0300

    ыв

--- src/Pages/MyHWs.js
+++ src/Pages/MyHWs.js
@@ -1,30 +1,6 @@
 const Themes = (props) => {
   return (
     <>
-      {/* <h1>Детали:</h1>
-      <ul>
-        <li>
-          <a href="https://stackblitz.com/edit/react-4bamwq?file=src%2Freadme.md">
-            Карточка 1:
-            https://stackblitz.com/edit/react-4bamwq?file=src%2Freadme.md
-          </a>
-        </li>
-
-        <li>
-          <a href="https://stackblitz.com/github/garlife/react3-hw2-6">
-            Карточка 2: https://stackblitz.com/github/garlife/react3-hw2-6
-          </a>
-        </li>
-        <li>
-          <a href="https://stackblitz.com/github/garlife/react3-hw3-1">Карточка 3:
-          https://stackblitz.com/github/garlife/react3-hw3-1</a>
-        </li>
-        <li>
-          <a href="https://stackblitz.com/github/garlife/react3-hw3-2">
-            Карточка 3: https://stackblitz.com/github/garlife/react3-hw3-2 
-          </a>
-        </li>
-      </ul> */}
           <h1>Пройденные темы</h1>
       <ol>
         <li>
